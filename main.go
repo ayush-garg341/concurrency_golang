97e1ebe13ccf2ab2703cac6608523889f4b40ed2
Ayush Garg <gargayush341@gmail.com>
2023-07-08 16:10:56 +0530

    unfair multi-threaded prime calculation

--- main.go
+++ main.go
@@ -2,12 +2,15 @@ package main
 
 import (
 	//race "concurrency_go/race"
-	lock "concurrency_go/locking"
+	//lock "concurrency_go/locking"
+	mt "concurrency_go/multithreading"
 )
 
 func main() {
 	//race.RaceConditions()
 	//race.RacyChannel()
 	//mt.CountSeq()
-	lock.Count()
+	//lock.Count()
+	mt.CountSeqUnfair()
+	//mt.CountSeq()
 }
--- multithreading/threaded_unfair.go
+++ multithreading/threaded_unfair.go
@@ -1,3 +1,49 @@
 package multithreading
 
-// Counting prime numbers upto 1 million sequentially..
+// Counting prime numbers upto 1 million threaded unfair.
+
+import (
+	"fmt"
+	"math"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+var numUnfair int = 1000000
+var countUnfair int32 = 0
+
+func CountSeqUnfair() {
+	begin := time.Now()
+
+	wg := sync.WaitGroup{}
+	start := 3
+	end := 100000
+	for i := 1; i <= 10; i++ {
+		wg.Add(1)
+		end = i * 100000
+		go threadedPrimeCount(&wg, start, end)
+		start = end + 1
+	}
+	wg.Wait()
+	elapsed := time.Since(begin)
+
+	fmt.Println("Final end value is ", end)
+	fmt.Println("Count of prime numbers is ", countUnfair+1, " time taken : ", elapsed)
+}
+
+func threadedPrimeCount(wg *sync.WaitGroup, start int, end int) {
+	for i := start; i <= end; i++ {
+		checkPrimeUnfair(i)
+	}
+	wg.Done()
+}
+
+func checkPrimeUnfair(num int) {
+	for i := 2; i <= int(math.Sqrt(float64(num))); i++ {
+		if num%i == 0 {
+			return
+		}
+	}
+	atomic.AddInt32(&countUnfair, 1)
+}
